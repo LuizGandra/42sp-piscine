8f828277ecfff454c95880873c72941e21534926
LuizGandra <luizc.gandra29@gmail.com>
2024-09-16 20:43:37 -0300

    Changed C 05 EX01.

--- c-05/ex01/ft_recursive_factorial.c
+++ c-05/ex01/ft_recursive_factorial.c
@@ -1,13 +1,10 @@
 int	ft_recursive_factorial(int nb)
 {
-	int	factorial;
-
-	factorial = nb;
 	if (!nb)
 		return (1);
 	if (nb < 0)
 		return (0);
-	return (factorial * ft_recursive_factorial(nb - 1));
+	return (nb * ft_recursive_factorial(nb - 1));
 }
 
 /*
