0623d34e2bc083a4ab2130d413e1423ca526443f
Luiz Gandra <luizc.gandra29@gmail.com>
2024-10-14 10:24:37 -0300

    Update ft_strlcpy.c

--- c-02/ex10/ft_strlcpy.c
+++ c-02/ex10/ft_strlcpy.c
@@ -9,7 +9,7 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	if (size > 0)
 	{
 		i = 0;
-		while (i < (size - 1))
+		while (src[i] && i < (size - 1))
 		{
 			dest[i] = src[i];
 			i++;
